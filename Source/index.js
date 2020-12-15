be4f7e95e470d6ce0488dff8ad8518cf3e679f09
Rahul <rahulsharma3258@gmail.com>
2020-12-15 15:15:51 +0530

    initial Commit

---
+++ Source/index.js
@@ -0,0 +1,17 @@
+
+// This Function Show & Hide the EXPLORE Menu On Mouse Click
+function showHide() 
+{
+    if(document.getElementById("3").style.visibility!=="hidden")
+    {
+        document.getElementById("3").style.visibility="hidden";
+    }
+    else
+    {
+        document.getElementById("3").style.visibility="visible";
+    }
+}
+
+
+
+
