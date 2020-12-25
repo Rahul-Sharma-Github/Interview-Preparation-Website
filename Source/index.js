fd31598f35a00aaca23a6bb972815c7717cea219
Rahul <rahulsharma3258@gmail.com>
2020-12-25 14:53:34 +0530

    Added Hover Effect on the <a> tag of Explore Menu, Added consol.log() Method in index.js file

--- Source/index.js
+++ Source/index.js
@@ -2,12 +2,19 @@
 // This Function Show & Hide the EXPLORE Menu On Mouse Click
 function showHide() 
 {
+
+    // to check the function is called or Not ? 
+    console.log("Function Called.");
+
+    // checking the visibility of explore menu 
+    // if visibility is not hidden then setting the visibility to hidden 
     if(document.getElementById("3").style.visibility!=="hidden")
     {
         document.getElementById("3").style.visibility="hidden";
     }
     else
     {
+        // else set visibility of explore menu to visible
         document.getElementById("3").style.visibility="visible";
     }
 }
