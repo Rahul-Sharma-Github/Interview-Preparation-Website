13872d4a82c5ecc3be160505dfb8f2992cd57b43
Rahul <rahulsharma3258@gmail.com>
2020-12-27 17:41:50 +0530

    Added a EventListner for Window Resize ..that calls the Welcome Function on Every Resize

--- Source/index.js
+++ Source/index.js
@@ -1,10 +1,22 @@
+
+//On Every Resize Event the  Welcome() Funtion Called
+window.addEventListener("resize", Welcome);
 // This Function Simply Replace the Navbar Menu's Text from "REGISTER HERE" to "REGISTER" for Better Mobile Experience
 function Welcome()
 {
     // just for checking function called or Not ?
     console.log("onload() Called..!");
 
-    // Storing "REGISTER HERE" into 'txt' Variable using ID  
+    
+
+    // Storing Browser Width in Variable 'vw'
+    let vw=document.documentElement.clientWidth;
+    console.log(vw);
+
+    // if Browser width is less than 575.98px then 
+    
+    if (vw<575.98) {
+        // Storing "REGISTER HERE" into 'txt' Variable using ID  
     let txt=document.getElementById("reg").innerText;
 
     console.log("text stored in txt variable =",txt);
@@ -20,7 +32,13 @@ function Welcome()
     // Finally Storing the new text(REGISTER)
     document.getElementById("reg").innerText=newtxt;
     console.log("Text Replaced with 'REGISTER'");
-
+    } else {
+        // if Browser width is greater than 757.98px than False part(else) executes
+        console.log("Client Width is Greater than 575.98px");
+        txt="REGISTER HERE"
+        document.getElementById("reg").innerText=txt;
+        console.log(txt);
+    }
 }
 
 
